b3e017d5f178731cdca94bed93b2a500cc7064db
hasanNaderi <hasannaderi27@gmail.com>
2025-07-28 16:38:40 +0330

    edit1

--- pages/products/[category]/index.js
+++ pages/products/[category]/index.js
@@ -1,51 +1,51 @@
-import { useRouter } from "next/router"
-import db from "@/data/db.json"
-import { useEffect, useState } from "react"
-import ProductCard from "@/components/productCard/ProductCard"
-import styles from "@/styles/ProductsByCategory.module.css"
+import { useRouter } from "next/router";
+import db from "@/data/db.json";
+import { useEffect, useState } from "react";
+import ProductCard from "@/components/productCard/ProductCard";
+import styles from "@/styles/ProductsByCategory.module.css";
 
 function ProductsByCategory() {
-   const { category } = useRouter().query
-   const [products, setProducts] = useState(db[category])
-   const [searchKey, setSearchKey] = useState("")
+  const { category } = useRouter().query;
+  const [products, setProducts] = useState([]);
+  const [searchKey, setSearchKey] = useState("");
 
-   useEffect(() => {
-      setProducts(db[category])
-      setSearchKey("")
-   }, [category])
+  useEffect(() => {
+    if (!category) return;
 
-   useEffect(() => {
-      if (searchKey) {
-         const searchProducts = db[category].filter((product) => product.text.includes(searchKey))
-         setProducts(searchProducts)
-      } else {
-         setProducts(db[category])
-      }
-   }, [searchKey])
+    if (searchKey) {
+      const searchProducts =
+        db[category]?.filter((product) => product.text.includes(searchKey)) ||
+        [];
+      setProducts(searchProducts);
+    } else {
+      setProducts(db[category] || []);
+    }
+  }, [searchKey, category]);
 
-   return (
-      <div className='container'>
-         <div className={`${styles.searchContainer} section`}>
-            <input
-               onChange={(e) => setSearchKey(e.target.value)}
-               type='text'
-               placeholder='دنبال چی میگردی؟'
-               className={styles.search}
-               value={searchKey}
-            />
-         </div>
-
-         <div className='section'>
-            <h1>محصولات ({category})</h1>
-            <div className='row'>
-               {products.map((product) => (
-                  <div className='col' key={product.id}>
-                     <ProductCard {...product} />
-                  </div>
-               ))}
-            </div>
-         </div>
+  return (
+    <div className="container">
+      <div className={`${styles.searchContainer} section`}>
+        <input
+          onChange={(e) => setSearchKey(e.target.value)}
+          type="text"
+          placeholder="دنبال چی میگردی؟"
+          className={styles.search}
+          value={searchKey}
+        />
       </div>
-   )
+
+      <div className="section">
+        <h1>محصولات ({category})</h1>
+        <div className="row">
+          {Array.isArray(products) &&
+            products.map((product) => (
+              <div className="col" key={product.id}>
+                <ProductCard {...product} />
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
 }
-export default ProductsByCategory
+export default ProductsByCategory;
